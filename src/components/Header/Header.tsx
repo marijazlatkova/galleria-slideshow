92b6362b42eda0f4892eed3327e64f367aeaa5fc
Marija Zlatkova <marijazlatkova44@yahoo.com>
2025-04-21 06:03:01 +0200

    clean header logic and structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,41 @@
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useSlideshow } from "../../hooks/useSlideshow";
 import { icons } from "../../db/icons";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
   const { open, setOpen } = useSlideshow();
+  const navigate = useNavigate();
 
   const toggleSlideshow = () => {
     setOpen(prev => {
       const next = !prev;
-      window.history.pushState(null, "", next ? "/slideshow/0" : "/");
+      navigate(next ? "/slideshow/0" : "/");
       return next;
     });
   };
 
+  const label = open ? "start slideshow" : "stop slideshow";
+
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <div className={styles.content}>
-        <Link to="/">
-          <img className={styles.logo} src={icons.logo} alt="Logo" />
+        <Link to="/" aria-label="go to homepage">
+          <img
+            className={styles.logo}
+            src={icons.logo}
+            alt="galleria logo"
+          />
         </Link>
         <button
           onClick={toggleSlideshow}
-          title={open ? "Stop Slideshow" : "Start Slideshow"}
           className={styles.slideshowBtn}
+          aria-label={label}
         >
-          {open ? "stop slideshow" : "start slideshow"}
+          {label}
         </button>
       </div>
-      <div className={styles.separator}></div>
-    </div>
+      <div className={styles.separator} />
+    </header>
   );
 };
