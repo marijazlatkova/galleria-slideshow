a832b8857e4d6482776fcc6fef3c35b002161f76
Marija Zlatkova <marijazlatkova44@yahoo.com>
2025-04-24 01:24:23 +0200

    add slideshow and update variable usage in header and gallery

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { SlideshowProvider } from "../providers/SlideshowProvider";
 import { Header } from "../components/Header/Header";
 import { Gallery } from "../components/Gallery/Gallery";
+import { Slideshow } from "../components/Slideshow/Slideshow";
 import styles from "./App.module.scss";
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Gallery />} />
-          <Route />
+          <Route path="/slideshow/:id" element={<Slideshow />} />
         </Routes>
       </SlideshowProvider>
     </div>
